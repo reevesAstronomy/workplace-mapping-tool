40fc9ffac88a5909ec47c78aa6b6937a71749228
Ubuntu <ubuntu@ip-172-26-6-143.ca-central-1.compute.internal>
2023-08-24 20:35:49 +0000

    Added .env file so that we can have different environments

--- union_drive_mapping/settings.py
+++ union_drive_mapping/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
