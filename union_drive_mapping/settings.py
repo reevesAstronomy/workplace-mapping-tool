b21150b739102f2c03f940b19f59d48ecadd0e10
Ubuntu <ubuntu@ip-172-26-6-143.ca-central-1.compute.internal>
2023-08-25 02:03:05 +0000

    small settings changes/tweaks - may break development server!

--- union_drive_mapping/settings.py
+++ union_drive_mapping/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,6 +85,8 @@ CHANNEL_LAYERS = {
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+#print('os.getenv', os.getenv('DB_NAME'))
 
 DATABASES = {
     'default': {
